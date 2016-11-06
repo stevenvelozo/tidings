4b9da28112b7ee56c461c4d8ecf12ad4499cb42f
Steven Velozo <steven@velozo.com>
2016-11-05 23:29:49 -0700

    Added module initialization

---
+++ InitializeOptionalModules.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+echo "Tidings Optional NPM Modules - Initialization"
+echo ""
+echo "--"
+echo ""
+
+echo "-> LaTeX"
+npm install latex-file
+
+echo "-> Graphviz"
+npm install graphviz
+
+echo "-> FFMPEG"
+npm install @ffmpeg-installer/ffmpeg fluent-ffmpeg @ffprobe-installer/ffprobe
+chmod +x node_modules/@ffprobe-installer/ffprobe/node_modules/@ffprobe-installer/linux-x64/ffprobe
+
+echo "-> WKHTMLTOPDF"
+npm install wkhtmltopdf-selfcontained
+
+echo "-> PhantomJS"
+npm install phantom-js-prebuilt phantom-html-to-pdf
---
+++ InitializeOptionalUbuntuSoftware.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Tidings Optional Ubuntu Module Software"
+echo ""
+echo "--"
+echo ""
+
+echo "-> LaTeX"
+sudo apt-get install texlive
+
+echo "-> Graphviz"
+sudo apt-get install graphviz 
