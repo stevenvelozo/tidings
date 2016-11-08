41358d533713bd161e648e17aeae4609a0df752f
jasonhillier <jason@paviasystems.com>
2016-11-08 11:59:57 -0800

    CommandLine rasterizer

---
+++ source/behaviors/renderRasterizers/rasterizeCommandLine.js
@@ -0,0 +1,44 @@
+// ##### Part of the **[retold](https://stevenvelozo.github.io/retold/)** system
+/**
+* @license MIT
+* @author <jason@hillier.us>
+*/
+var libFS = require('fs');
+var libChildProcess = require('child_process');
+
+module.exports = (pTaskData, pState, fCallback) =>
+{
+	var tmpFileName = pTaskData.File;
+
+	// If no path was supplied, use the Stage path
+	if (!pTaskData.Path)
+		pTaskData.Path = pState.Manifest.Metadata.Locations.Stage;
+
+	if (!pTaskData.OutputPath)
+		pTaskData.OutputPath = pState.Manifest.Metadata.Locations.Stage;
+	
+	if (!pTaskData.WorkDir)
+		pTaskData.WorkDir = `${pState.Manifest.Metadata.Locations.Renderer}`;
+	
+	if ((typeof(pTaskData.Options) === 'object') && (typeof(pTaskData.Options.Configuration) === 'object'))
+	{
+		var tmpParameterString = '';
+		if (pTaskData.Options.Configuration.Parameters)
+			tmpParameterString = pTaskData.Options.Configuration.Parameters.join(' ');
+		
+		if (!pTaskData.Command)
+			pTaskData.Command = `${pTaskData.Options.Configuration.Program} ${tmpParameterString}`;
+	}
+	
+	libChildProcess.exec(pTaskData.Command, { cwd: pTaskData.WorkDir },
+		function(pError, pStdout, pStderr)
+		{
+			if (pError)
+				pState.Behaviors.stateLog(pState, 'Error executing command line program: '+JSON.stringify(pTaskData)+' '+pError, true);
+			
+			pState.Behaviors.stateLog(pState, 'STDOUT: '+pStdout, true);
+			pState.Behaviors.stateLog(pState, 'STDERR: '+pStderr, true);
+
+			return fCallback(null, pState);
+		});
+};
